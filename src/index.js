a2923b8ff9deec2c28870c2fa718d96071473966
André Carvalho <andre.vrlcarvalho@hotmail.com>
2023-03-29 10:46:01 -0300

    criando um hook personalizado para useContext envolver um erro personalizado para informar devs q forem trabalhar com ele, como deve ser utilizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CounterContextProvider } from './contexts/CounterContext';
 import './styles/global-styles.css';
 import { Home } from './templates/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(
+  <React.StrictMode>
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
+  </React.StrictMode>,
+);
